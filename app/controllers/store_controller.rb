17bd637efd43fda427b6cb5edd2b042541ea091c
jelena <mail+github@jelena.io>
2018-11-27 16:51:08 +0000

    added visit counter to index

--- app/controllers/store_controller.rb
+++ app/controllers/store_controller.rb
@@ -1,5 +1,14 @@
 class StoreController < ApplicationController
   def index
+    def counter
+      if session[:counter].nil?
+        session[:counter] = 0
+      else
+        session[:counter] += 1
+      end
+    end
+
     @products = Product.order(:title)
+    @show_message = 'You visited this page ' + counter.to_s + ' times'
   end
 end
